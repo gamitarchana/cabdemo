73f934352d1bacae6603942c63eb8245463f134f
gamitarchana <45191739+gamitarchana@users.noreply.github.com>
2019-06-03 12:44:26 +0530

    Update wsgi.py
    updated wsgi settings for new domain

--- cabdemo/wsgi.py
+++ cabdemo/wsgi.py
@@ -11,6 +11,17 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cabdemo.settings.dev")
+sys.path.append('/var/www/html/outstation/projects/cabdemo/cabdemo') 
+# adjust the Python version in the line below as needed 
+sys.path.append('/var/www/html/outstation/projects/cabdemo_env/lib/python3.6/site-packages') 
 
-application = get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cabdemo.settings.production")
+
+try: 
+    application = get_wsgi_application() 
+except Exception: 
+    # Error loading applications 
+    if 'mod_wsgi' in sys.modules: 
+        traceback.print_exc() 
+        os.kill(os.getpid(), signal.SIGINT) 
+        time.sleep(2.5) 
