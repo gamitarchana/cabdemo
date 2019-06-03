e8d0c0fe0af9fc50817513f5bc83aab1ffb43846
gamitarchana <45191739+gamitarchana@users.noreply.github.com>
2019-06-03 12:45:56 +0530

    Update production.py
    Added new domain

--- cabdemo/settings/production.py
+++ cabdemo/settings/production.py
@@ -2,6 +2,14 @@ from .base import *
 
 DEBUG = False
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '7@-qr1ld5e44q@m@!zegp#6dzfak9)yuua#p0md4@v#n$o*&-x'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['dolfin.merucabs.com:5443/outstationdemo'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 try:
     from .local import *
 except ImportError:
