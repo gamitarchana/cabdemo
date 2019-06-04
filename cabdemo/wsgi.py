7fa39e50d0665a75a265e367eb87a00b21f60790
gamitarchana <45191739+gamitarchana@users.noreply.github.com>
2019-06-04 16:30:57 +0530

    Update wsgi.py
    modified sys path for project directory and virtual env

--- cabdemo/wsgi.py
+++ cabdemo/wsgi.py
@@ -8,20 +8,14 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append('/var/www/html/cabdemo/cabdemo')
+sys.path.append('/var/www/html/cabdemo/cabdemo_env')
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('/var/www/html/outstation/projects/cabdemo/cabdemo') 
-# adjust the Python version in the line below as needed 
-sys.path.append('/var/www/html/outstation/projects/cabdemo_env/lib/python3.6/site-packages') 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cabdemo.settings.production')
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cabdemo.settings.production")
+application = get_wsgi_application()
 
-try: 
-    application = get_wsgi_application() 
-except Exception: 
-    # Error loading applications 
-    if 'mod_wsgi' in sys.modules: 
-        traceback.print_exc() 
-        os.kill(os.getpid(), signal.SIGINT) 
-        time.sleep(2.5) 
